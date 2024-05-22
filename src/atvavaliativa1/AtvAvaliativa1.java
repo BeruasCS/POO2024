baf7384b1cefb9e43d32e617a6909f7323f2d9da
BeruasCS <bernardoruas10@gmail.com>
2024-05-22 14:01:45 -0300

    Pratica 1

--- src/atvavaliativa1/AtvAvaliativa1.java
+++ src/atvavaliativa1/AtvAvaliativa1.java
@@ -14,7 +14,9 @@ public class AtvAvaliativa1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pratica1TemperaturaInstancia tempC = new Pratica1TemperaturaInstancia(25.0);
+        System.out.println("Celsius para Kelvin: " + tempC.celciusKelvin());
+        System.out.println("kelvin para celcius"+ tempC.kelvinCelcius());
     }
     
 }
