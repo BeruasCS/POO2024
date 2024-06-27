c2fb649ee9bb24f1171c1cd928ed2d1bc28fa2e5
BeruasCS <bernardoruas10@gmail.com>
2024-06-27 02:20:58 -0300

    Pratica 1

--- src/atvavaliativa1/ValidadorCPFCNPJ.java
+++ src/atvavaliativa1/ValidadorCPFCNPJ.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package atvavaliativa1;
 
 /**
@@ -9,5 +10,7 @@ package atvavaliativa1;
  * @author beruas
  */
 public class ValidadorCPFCNPJ {
-    
+    public int obterDigito(long cpf, int pos){
+        return (int) ((cpf/ Math.pow(10, pos)) % 10);
+    }
 }
