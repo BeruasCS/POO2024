68c6c4e4a64065463c5ea4a8c03e706db01deb68
BeruasCS <bernardoruas10@gmail.com>
2024-07-02 07:57:31 -0300

    Pratica 1

--- src/atvavaliativa1/ValidadorCPFCNPJ.java
+++ src/atvavaliativa1/ValidadorCPFCNPJ.java
@@ -10,21 +10,21 @@ package atvavaliativa1;
  * @author beruas
  */
 public class ValidadorCPFCNPJ {
-    public static int obterDigito(long cpf, int pos){
-        return (int) ((cpf/ Math.pow(10, pos)) % 10);
-    }
-        public static boolean isCpfValido(long cpf) {
-             int z = 0;  
-             int n;
-             int soma = 0;
-          for (int i = 10; i >= 2; i--) {
-            
-            n= obterDigito(cpf, z);
-           soma = soma +(n*i);
-           z++;
-            }
 
-        return true; }
+    public static int obterDigito(long cpf, int pos){
+        return (int) ((cpf / (long) Math.pow(10, pos)) % 10);
+    }
     
+    public static boolean isCpfValido(long cpf) {
+        int z = 0;  
+        int n;
+        int soma = 0;
+        for (int i = 10; i >= 2; i--) {
+            n = obterDigito(cpf, z);
+            soma = soma + (n * i);
+            z++;
+        }
     
+        return true; 
+    }
 }
