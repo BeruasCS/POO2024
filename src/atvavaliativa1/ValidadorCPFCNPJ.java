47ab5e31f98d6d37036e057708209049b66288ad
BeruasCS <bernardoruas10@gmail.com>
2024-07-03 21:25:04 -0300

    Pratica 1

--- src/atvavaliativa1/ValidadorCPFCNPJ.java
+++ src/atvavaliativa1/ValidadorCPFCNPJ.java
@@ -16,21 +16,33 @@ public class ValidadorCPFCNPJ {
     
     public static boolean isCpfValido(long cpf) {
         
-          if (cpf < 10000000000L || cpf > 99999999999L) {
+        if (cpf < 10000000000L || cpf > 99999999999L) {
             return false;
         }
-          
-      int soma = 0;
-       for (int i = 0; i < 9; i++) {
+
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
             soma += obterDigito(cpf, 10 - i) * (10 - i);
         }
         int primeiroDigitoVerificador = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+
         soma = 0;
-      for (int i = 0; i < 10; i++) {
-            soma += obterDigito(cpf, 11 - i) * (11 - i);
+        for (int i = 0; i < 9; i++) {
+            soma += obterDigito(cpf, 10 - i) * (11 - i);
         }
+        soma += primeiroDigitoVerificador * 2;
         int segundoDigitoVerificador = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
-      return (obterDigito(cpf, 1) == primeiroDigitoVerificador) &&
-               (obterDigito(cpf, 0) == segundoDigitoVerificador);
+
+        return obterDigito(cpf, 1) == primeiroDigitoVerificador &&
+                obterDigito(cpf, 0) == segundoDigitoVerificador;
+    }
+
+    public static boolean isCpfValido(String cpf) {
+        try {
+            long cpfLong = Long.parseLong(cpf);
+            return isCpfValido(cpfLong);
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
