740b47a6c663ca0eee74b7984ee6a23d05847e0d
BeruasCS <bernardoruas10@gmail.com>
2024-07-05 14:40:34 -0300

    Pratica 1

--- src/atvavaliativa1/PasseioCavaloSemHeurística.java
+++ src/atvavaliativa1/PasseioCavaloSemHeurística.java
@@ -10,4 +10,14 @@ package atvavaliativa1;
  */
 public class PasseioCavaloSemHeurística {
     
+    int currentColumn; //a coluna em que o cavalo esta no momento
+    int currentRow; //a linha que o equino esta no momento 
+    int moveNumber; 
+    int[] vertical = {-1,-2,-2,-1,1,2,2,1}; //movimento a ser feito na vertical
+    int[] horizontal = {2,1,-1,-2,-2,-1,1,2}; //movimento na horizontal
+    int[][] tabuleiro= new int[8][8]; //o tabuleiro, onde 0= livre, 1= ocupada
+ /*moveNumber sera o indice do for, e ele indicara  qual posicao do
+ array vertical ou horizontal escolheremos, e nseesaposicao temos o 
+ movimento que faremos*/
+    
 }
