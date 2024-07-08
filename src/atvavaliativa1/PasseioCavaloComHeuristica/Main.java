9a5f76ea002b6e506ff47fa2af3451eda72a2ce6
BeruasCS <bernardoruas10@gmail.com>
2024-07-07 23:19:32 -0300

    Pratica 1

---
+++ src/atvavaliativa1/PasseioCavaloComHeuristica/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atvavaliativa1.PasseioCavaloComHeuristica;
+
+/**
+ *
+ * @author beruas
+ */
+public class Main {
+    
+   public static void main(String[] args) {
+
+        PasseioCavaloComHeuristica knight= new PasseioCavaloComHeuristica(0,0);
+        //Coloque os valores inicias de onde o cavalo começa
+        
+        knight.tour();
+    }
+}
